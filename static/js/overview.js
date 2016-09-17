f26c06e4f7ab783adcd6394fa4e182f966eb1bd2
Marc Gähwiler <marc@marcg.ch>
2016-09-17 20:06:26 +0200

    Map stuff

--- static/js/overview.js
+++ static/js/overview.js
@@ -1,10 +1,31 @@
 function initMap() {
-  var var_location = new google.maps.LatLng(45.430817,12.331516);
+    var var_location = new google.maps.LatLng(45.430817, 12.331516);
 
 
-  var mapDiv = document.getElementById('map');
-  var map = new google.maps.Map(mapDiv, {
-      center: {lat: 44.540, lng: -78.546},
-      zoom: 8
-  });
+    var mapDiv = document.getElementById('map');
+    var map = new google.maps.Map(mapDiv, {
+        center: {lat: 47.39, lng: 8.515},
+        zoom: 15,
+        styles: [
+            {
+                featureType: 'all',
+                stylers: [
+                    {saturation: -80}
+                ]
+            }, {
+                featureType: 'road.arterial',
+                elementType: 'geometry',
+                stylers: [
+                    {hue: '#00ffee'},
+                    {saturation: 50}
+                ]
+            }, {
+                featureType: 'poi.business',
+                elementType: 'labels',
+                stylers: [
+                    {visibility: 'off'}
+                ]
+            }
+        ]
+    });
 }
